8fe23a12ed4bc7643718b055ee404767c460c067
Charles Gichuki <sasagichuki@gmail.com>
2013-12-04 23:27:03 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kumbukumbu::Application.config.secret_key_base = 'c1f6c223677a21131ab5d060606425b67d4adc3d3d2335cbe0c61c56c470d177c46c42ff9b9c8264d5a4ec7e11af7a5981b06ba92a1cbab181a988633d4f1c30'
