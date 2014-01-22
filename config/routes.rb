fbffc6da943de214ddd2e4a372ea6aa56c0a86da
Charles Gichuki <Charles.gichuki@squaddigital.com>
2014-01-22 21:09:30 +0300

    Styled Listings page, added seed data

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Kumbukumbu::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root 'listings#index'
 
   # Example of regular route:
   #   get 'products/:id'  => 'catalog#view'
