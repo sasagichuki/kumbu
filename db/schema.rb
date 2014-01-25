2ac8c69d667da5a81bd970519451b6dcc1169ef8
Charles Gichuki <Charles.gichuki@squaddigital.com>
2014-01-25 21:00:07 +0300

    Moved to dev db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140124210944) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: true do |t|
     t.string   "title"
     t.text     "description"
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 20140124210944) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
