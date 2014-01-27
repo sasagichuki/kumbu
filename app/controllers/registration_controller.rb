30c0d8bb3f6d315c69875585d6a9c333c5b5e8f4
Charles Gichuki <Charles.gichuki@squaddigital.com>
2014-01-28 00:40:44 +0300

    Style changes to index, I18n.enforce to false commented validation to seed db

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,23 +1,23 @@
 class RegistrationController < ApplicationController
-  # def new
-  #   @user = User.new
-  # end
+    # def new
+    #   @user = User.new
+    # end
 
-  # def create
-  #   @user = User.new
-  #   @user.username = params[:user][:username]
-  #   @user.email = params[:user][:email]
-  #   @user.password = params[:user][:password]
-  #   @user.password_confirmation =params[:user][:password_confirmation]
+    # def create
+    #   @user = User.new
+    #   @user.username = params[:user][:username]
+    #   @user.email = params[:user][:email]
+    #   @user.password = params[:user][:password]
+    #   @user.password_confirmation =params[:user][:password_confirmation]
 
-  #   @user.valid?
-  #   if @user.errors.blank?
+    #   @user.valid?
+    #   if @user.errors.blank?
 
-  #   @user.save
+    #   @user.save
 
-  #   redirect_to listings_path
-  # else
-  #   :action => "new"
-  # end
-  # end
+    #   redirect_to listings_path
+    # else
+    #   :action => "new"
+    # end
+    # end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,11 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :photo, :styles => { :small => "150x150>", :medium => "300x300", :thumb => "100x100>"},
+  has_attached_file :photo, :styles => { :small => "150x150>", :medium => "300x300#", :thumb => "100x100>"},
                     :url => "/assets/listings/:id/:style/:basename.:extension",
                     :path => ":rails_root/public/assets/listings/:id/:style/:basename.:extension"
 
-  validates_attachment_presence :photo
+  # validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg','image/png']
+  # validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'
+
 end
